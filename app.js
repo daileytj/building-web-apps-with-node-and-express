d23ef6004a1c8ae4a61e92fb81ab2ea1687a924c
daileytj <daileytj89@gmail.com>
2021-08-09 06:49:44 -0400

    Add conference website template files

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require('express');
 const chalk = require('chalk'); // terminal string styling
 const debug = require('debug')('app'); // debugging utility - run 'DEBUG=* node app.js' in terminal to run in debug mode with all message || 'DEBUG=app node app.js' to debug app.js file
 const morgan = require('morgan'); // HTTP request logger middleware for node.js
-const path = require('path')
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('tiny')); // 'combined' gives us a lot of information, 'tiny' gives us minimal information
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.static(path.join(__dirname, '/public/'))); // middleware that will return index.html from /public/ if it exists, if it doesn't exist, continue...
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 // req = request, res = response
 app.get('/', (req, res) => {
-  res.send('Hello from my app');
+  res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
 });
 
 app.listen(PORT, () => {
